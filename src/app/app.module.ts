48c5422102e82c6a3505d430e90ea1aee144c0da
rubenfgr87 <rubenfgr87@outlook.com>
2020-05-20 11:23:11 +0200

    Inicio proyecto base para trabajar con Socket.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  constructor(private socket: Socket) { }
+}
